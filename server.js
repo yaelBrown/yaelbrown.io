6c44774376b14c8b0012a64c839e4aedfe8bb4fa
Yael Brown <yaelrbrown@gmail.com>
2019-01-24 04:49:15 -0500

    npm init, created server.js, installed packages
    Initialized project with npm. Installed nodemon, bootstrap and express. Created server.js and worked on alt-index.html. Need to create thumbs for current projects.

---
+++ server.js
@@ -0,0 +1,10 @@
+// yaelbrown.io/server.js 
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + '/'));
+app.get('/', (req, res) => res.sendfile(__dirname + '/public/index.html'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
